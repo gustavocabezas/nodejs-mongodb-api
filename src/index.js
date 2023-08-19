1c105eba8c3e7a9cf1fd67cf5546737d2dff6744
gustavocabezasx <gustavocabezasx@gmail.com>
2023-08-18 23:52:25 -0600

    add mssql

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const sql = require('mssql');
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
@@ -11,21 +12,25 @@ const orderRoute = require("./routes/order");
 const orderProductRoute = require("./routes/orderProduct");
 const productRoute = require("./routes/product");
 
-const app = express();  
+const app = express();
 
 app.get('/', (req, res) => {
     res.send("Welcome to my API");
 })
-app.use('/', securityRoutes); 
+app.use('/', securityRoutes);
 app.use('/', userRoutes);
 app.use('/', customerRoutes);
 app.use('/', orderRoute);
 app.use('/', orderProductRoute);
 app.use('/', productRoute);
-  
+
 mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => console.log("Connected to MongoDB Atlas"))
     .catch((error) => console.error(error));
+
+sql.connect(process.env.MSSQL_CONNECTION_STRING)
+    .then(() => console.log("Connected to MSSQL"))
+    .catch(error => console.error("Error connecting to MSSQL:", error));
 
 module.exports = app;
